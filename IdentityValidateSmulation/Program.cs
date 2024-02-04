4b88f1eab5539a2731477f3149661c8be71b4cd7
serbaysarikaya <ssarikaya@ritma.com.tr>
2024-02-04 15:42:08 +0300

    Add project files.

---
+++ IdentityValidateSmulation/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace IdentityValidateSmulation
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CitizenManager citizenManager = new CitizenManager(new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap));
+            Citizen citizen = new Citizen();
+
+            Console.Write("TC No: ");
+            citizen.IdentityNumber = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("Ad: ");
+            citizen.Name = Console.ReadLine();
+
+            Console.Write("Soyad: ");
+            citizen.Surname = Console.ReadLine();
+
+            Console.Write("Doğum Yılı: ");
+            citizen.BirthYear = Convert.ToInt32(Console.ReadLine());
+
+            citizenManager.Validate(citizen);
+
+            Console.WriteLine("\nDevam etmek için lütfen enter tuşuna basınız...");
+            Console.ReadLine();
+        }
+    }
+}
